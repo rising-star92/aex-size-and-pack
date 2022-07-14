24614d1d4d3e737fb2782f5541ebd6a717d26ef3
rising-star92 <ryan.service.1001@gmail.com>
2022-07-14 18:34:44 +0530

    changes in stylelist dto

--- src/main/java/com/walmart/aex/sp/dto/StyleList.java
+++ src/main/java/com/walmart/aex/sp/dto/StyleList.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Data
 public class StyleList {
 
-    private Integer styleNbr;
+    private String styleNbr;
     private MetricsDto metricsObj;
     private List<ClustersDto> clusterList;
 
