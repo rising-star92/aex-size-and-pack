163deed5ea1becf2257b285d1fb5053a0175b033
rising-star92 <ryan.service.1001@gmail.com>
2022-08-05 09:34:29 -0500

    Add strategy service call and Calculate Buy Qty Controller

---
+++ src/main/java/com/walmart/aex/sp/dto/buyquantity/CalculateBuyQtyRequest.java
@@ -0,0 +1,15 @@
+package com.walmart.aex.sp.dto.buyquantity;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CalculateBuyQtyRequest {
+    private Long planId;
+    private String channel;
+    private Integer lvl0Nbr;
+    private Integer lvl1Nbr;
+    private Integer lvl2Nbr;
+    private List<Lvl3Dto> lvl3List;
+}
