86e5b627e122016485f36b353c5105690655a653
rising-star92 <ryan.service.1001@gmail.com>
2022-08-11 11:03:35 +0530

    Comments resolved

--- src/main/java/com/walmart/aex/sp/dto/buyquantity/CustomerChoiceDto.java
+++ src/main/java/com/walmart/aex/sp/dto/buyquantity/CustomerChoiceDto.java
@@ -1,21 +1,18 @@
 package com.walmart.aex.sp.dto.buyquantity;
 
-import java.util.List;
-
-import com.walmart.aex.sp.dto.packoptimization.FixtureDto;
 import com.walmart.aex.sp.dto.replenishment.MerchMethodsDto;
-
 import lombok.Data;
+
+import java.util.List;
 
 @Data
 public class CustomerChoiceDto {
 
-	private String ccId;
+    private String ccId;
     private String colorName;
     private MetricsDto metrics;
     private List<ClustersDto> clusters;
     private List<MerchMethodsDto> merchMethods;
- 
 
 
 }
--- src/main/java/com/walmart/aex/sp/dto/buyquantity/FinelineDto.java
+++ src/main/java/com/walmart/aex/sp/dto/buyquantity/FinelineDto.java
@@ -7,11 +7,11 @@ import java.util.List;
 
 @Data
 public class FinelineDto {
-	 private Integer finelineNbr;
-	    private String finelineDesc;
-	    private String finelineAltDesc;
-	    private MetricsDto metrics;
-	    private List<ClustersDto> clusters;
-	    private List<StyleDto> styles;
-	    private List<MerchMethodsDto> merchMethods;
+    private Integer finelineNbr;
+    private String finelineDesc;
+    private String finelineAltDesc;
+    private MetricsDto metrics;
+    private List<ClustersDto> clusters;
+    private List<StyleDto> styles;
+    private List<MerchMethodsDto> merchMethods;
 }
