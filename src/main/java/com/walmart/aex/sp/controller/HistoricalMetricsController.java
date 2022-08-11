c99e358a2f44df7e5726d7153861e39b3e48f72a
rising-star92 <ryan.service.1001@gmail.com>
2022-08-11 13:56:22 -0500

    Adding controller logic

--- src/main/java/com/walmart/aex/sp/controller/HistoricalMetricsController.java
+++ src/main/java/com/walmart/aex/sp/controller/HistoricalMetricsController.java
@@ -2,19 +2,24 @@ package com.walmart.aex.sp.controller;
 
 import com.walmart.aex.sp.dto.historicalmetrics.HistoricalMetricsRequest;
 import com.walmart.aex.sp.dto.historicalmetrics.HistoricalMetricsResponse;
+import com.walmart.aex.sp.service.HistoricalMetricsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HistoricalMetricsController {
 
+   @Autowired
+   HistoricalMetricsService historicalMetricsService;
+
    @QueryMapping
    public HistoricalMetricsResponse fetchHistoricalMetricsFineline(HistoricalMetricsRequest request) {
-      return null;
+      return historicalMetricsService.fetchHistoricalMetricsFineline(request);
    }
 
    @QueryMapping
    public HistoricalMetricsResponse fetchHistoricalMetricsCC(HistoricalMetricsRequest request) {
-      return null;
+      return historicalMetricsService.fetchHistoricalMetricsCC(request);
    }
 }
