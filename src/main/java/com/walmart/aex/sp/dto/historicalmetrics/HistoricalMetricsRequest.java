d0c8c0792bbd86cc9012d2ebd6036d6de69025ba
rising-star92 <ryan.service.1001@gmail.com>
2022-08-17 13:36:38 -0500

    Fixed

--- src/main/java/com/walmart/aex/sp/dto/historicalmetrics/HistoricalMetricsRequest.java
+++ src/main/java/com/walmart/aex/sp/dto/historicalmetrics/HistoricalMetricsRequest.java
@@ -5,6 +5,9 @@ import lombok.Data;
 @Data
 public class HistoricalMetricsRequest {
    private Integer planId;
+   private Integer lvl0Nbr;
+   private Integer lvl1Nbr;
+   private Integer lvl2Nbr;
    private Integer lvl3Nbr;
    private Integer lvl4Nbr;
    private Integer finelineNbr;
