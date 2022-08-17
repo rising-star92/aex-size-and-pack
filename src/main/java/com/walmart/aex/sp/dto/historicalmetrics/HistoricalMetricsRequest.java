11f09af15a814ef9f34f9a5b54d47bb03a106f88
rising-star92 <ryan.service.1001@gmail.com>
2022-08-17 13:18:49 -0500

    Removing l0/1/2 from DTO

--- src/main/java/com/walmart/aex/sp/dto/historicalmetrics/HistoricalMetricsRequest.java
+++ src/main/java/com/walmart/aex/sp/dto/historicalmetrics/HistoricalMetricsRequest.java
@@ -5,9 +5,6 @@ import lombok.Data;
 @Data
 public class HistoricalMetricsRequest {
    private Integer planId;
-   private Integer lvl0Nbr;
-   private Integer lvl1Nbr;
-   private Integer lvl2Nbr;
    private Integer lvl3Nbr;
    private Integer lvl4Nbr;
    private Integer finelineNbr;
