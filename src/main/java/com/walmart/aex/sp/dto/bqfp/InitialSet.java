9c4a772cc67c9c73044206780011d198ce887c5e
rising-star92 <ryan.service.1001@gmail.com>
2022-08-29 17:20:23 -0500

    Update store buy qty and Bumpset qty

--- src/main/java/com/walmart/aex/sp/dto/bqfp/InitialSet.java
+++ src/main/java/com/walmart/aex/sp/dto/bqfp/InitialSet.java
@@ -1,8 +1,10 @@
 package com.walmart.aex.sp.dto.bqfp;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class InitialSet {
    private Integer weeksOfSale;
    private Long initialSetUnitsPerFix;
