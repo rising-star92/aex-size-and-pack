95b3b3c36cab0296018c91f17cd8540886b519f2
rising-star92 <ryan.service.1001@gmail.com>
2022-08-31 14:20:29 -0400

    adding deleteLinePlan API stub

---
+++ src/main/java/com/walmart/aex/sp/dto/planhierarchy/PlanSizeAndPackDeleteDTO.java
@@ -0,0 +1,9 @@
+package com.walmart.aex.sp.dto.planhierarchy;
+
+import lombok.Data;
+
+@Data
+public class PlanSizeAndPackDeleteDTO {
+    private StrongKey strongKey;
+    private PlanSizeAndPackDTO sizeAndPackPayloadDTO;
+}
---
+++ src/main/java/com/walmart/aex/sp/dto/planhierarchy/StrongKey.java
@@ -0,0 +1,14 @@
+package com.walmart.aex.sp.dto.planhierarchy;
+
+import com.walmart.aex.sp.dto.packoptimization.Fineline;
+
+public class StrongKey {
+    private Long planId;
+    private String planDesc;
+    private Integer lvl0Nbr;
+    private Integer lvl1Nbr;
+    private Integer lvl2Nbr;
+    private Integer lvl3Nbr;
+    private Integer lvl4Nbr;
+    private Fineline fineline;
+}
