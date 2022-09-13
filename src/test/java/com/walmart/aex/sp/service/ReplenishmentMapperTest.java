7b4ffc2192cec66048562adc126e1dadde92c0ce
rising-star92 <ryan.service.1001@gmail.com>
2022-09-13 16:53:31 +0530

    code changes for junit

--- src/test/java/com/walmart/aex/sp/service/ReplenishmentMapperTest.java
+++ src/test/java/com/walmart/aex/sp/service/ReplenishmentMapperTest.java
@@ -1,46 +1,71 @@
 package com.walmart.aex.sp.service;
 
-import com.walmart.aex.sp.dto.replenishment.ReplenishmentResponse;
-import com.walmart.aex.sp.dto.replenishment.ReplenishmentResponseDTO;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.walmart.aex.sp.dto.replenishment.ReplenishmentResponse;
+import com.walmart.aex.sp.dto.replenishment.ReplenishmentResponseDTO;
+
 @ExtendWith(MockitoExtension.class)
 public class ReplenishmentMapperTest {
 
-        @InjectMocks
-        ReplenishmentMapper replenishmentMapper;
-        @Mock
-        ReplenishmentResponseDTO replenishmentResponseDTO;
-        private static final Integer finelineNbr=3470;
-        private static final Long planId=471l;
-        @Test
-        public void testMapReplenishmentLvl2Sp() {
+	@InjectMocks
+	ReplenishmentMapper replenishmentMapper;
 
-            ReplenishmentResponse replenishmentResponse= new ReplenishmentResponse();
-            replenishmentResponseDTO=new ReplenishmentResponseDTO();
-            replenishmentResponseDTO.setChannelId(2);
-            replenishmentResponseDTO.setPlanId(planId);
-            replenishmentResponseDTO.setMerchMethod(1);
-            replenishmentMapper.mapReplenishmentLvl2Sp(replenishmentResponseDTO,replenishmentResponse,finelineNbr,"black");
-            assertNotNull(replenishmentResponse);
-            assertEquals(replenishmentResponse.getPlanId(),471l);
-        }
-        
-        @Test
-        public void testMapReplenishmentLvl4Sp() {
+	@Mock
+	ReplenishmentResponseDTO replenishmentResponseDTO;
 
-            ReplenishmentResponse replenishmentResponse= new ReplenishmentResponse();
-            replenishmentResponseDTO=new ReplenishmentResponseDTO();
-            replenishmentResponseDTO.setChannelId(2);
-            replenishmentResponseDTO.setPlanId(planId);
-            replenishmentResponseDTO.setMerchMethod(1);
-            replenishmentMapper.mapReplenishmentLvl2Sp(replenishmentResponseDTO,replenishmentResponse,null,null);
-            assertNotNull(replenishmentResponse);
-            assertEquals(replenishmentResponse.getPlanId(),471l);
-        }    
+	private static final Integer finelineNbr = 3470;
+	private static final Long planId = 471l;
+
+	private static final Integer replPack = 4701;
+	private static final Integer finalBQY = 76064;
+	private static final Double PackRation = 3.0;
+
+	@Test
+	public void testMapReplenishmentLvl2Sp() {
+
+		ReplenishmentResponse replenishmentResponse = new ReplenishmentResponse();
+		replenishmentResponseDTO = new ReplenishmentResponseDTO();
+		replenishmentResponseDTO.setChannelId(2);
+		replenishmentResponseDTO.setPlanId(planId);
+		replenishmentResponseDTO.setMerchMethod(1);
+		replenishmentMapper.mapReplenishmentLvl2Sp(replenishmentResponseDTO, replenishmentResponse, finelineNbr,
+				"black");
+		assertNotNull(replenishmentResponse);
+		assertEquals(replenishmentResponse.getPlanId(), 471l);
+	}
+
+	@Test
+	public void testMapReplenishmentLvl4Sp() {
+
+		ReplenishmentResponse replenishmentResponse = new ReplenishmentResponse();
+		replenishmentResponseDTO = new ReplenishmentResponseDTO();
+		replenishmentResponseDTO.setChannelId(2);
+		replenishmentResponseDTO.setPlanId(planId);
+		replenishmentResponseDTO.setMerchMethod(1);
+		replenishmentResponseDTO.setLvl3finalBuyQty(finalBQY);
+		replenishmentResponseDTO.setLvl3vnpkWhpkRatio(PackRation);
+		replenishmentResponseDTO.setLvl3ReplPack(replPack);
+
+		//passing finelineNBR and CCID as null so that finalBQY,PackRation,ReplenishmentPacks are set 
+		replenishmentMapper.mapReplenishmentLvl2Sp(replenishmentResponseDTO, replenishmentResponse, null, null);
+		assertNotNull(replenishmentResponse);
+		assertEquals(replenishmentResponse.getPlanId(), 471l);
+		assertNotNull(replenishmentResponse.getLvl3List());
+		
+		Integer finalBuyQty = replenishmentResponse.getLvl3List().get(0).getMetrics().getFinalBuyQty();
+		Double vpnkWhpkRation = replenishmentResponse.getLvl3List().get(0).getMetrics().getPackRatio();
+		Integer replenishmentPack = replenishmentResponse.getLvl3List().get(0).getMetrics().getReplenishmentPacks();
+
+		assertEquals(finalBuyQty, finalBQY);
+		assertEquals(vpnkWhpkRation, PackRation);
+		assertEquals(replenishmentPack, replPack);
+	}
 }
