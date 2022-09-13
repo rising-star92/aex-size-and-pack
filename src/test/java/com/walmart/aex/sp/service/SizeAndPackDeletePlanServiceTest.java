677c3082bbb2c414800f3f1a43c192d771c6c24a
rising-star92 <ryan.service.1001@gmail.com>
2022-09-13 10:04:26 -0400

    added unit test

---
+++ src/test/java/com/walmart/aex/sp/service/SizeAndPackDeletePlanServiceTest.java
@@ -0,0 +1,81 @@
+package com.walmart.aex.sp.service;
+
+import com.walmart.aex.sp.dto.packoptimization.CustomerChoice;
+import com.walmart.aex.sp.dto.packoptimization.Fineline;
+import com.walmart.aex.sp.dto.planhierarchy.*;
+import com.walmart.aex.sp.entity.MerchCatPlan;
+import com.walmart.aex.sp.repository.MerchCatPlanRepository;
+import com.walmart.aex.sp.util.SizeAndPackTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.*;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doReturn;
+
+@ExtendWith(MockitoExtension.class)
+public class SizeAndPackDeletePlanServiceTest {
+
+    @InjectMocks
+    private SizeAndPackDeletePlanService sizeAndPackDeletePlanService;
+
+    @Mock
+    private MerchCatPlanRepository merchCatPlanRepository;
+
+    @Test
+    void testUpdateMerchCatPlan() {
+        //Arrange
+        PlanSizeAndPackDTO request = new PlanSizeAndPackDTO();
+        request.setPlanId(471l);
+        request.setLvl0Nbr(50000);
+
+        List<Lvl1> lvl1List = new ArrayList<>();
+        Lvl1 lvl1 = new Lvl1();
+        lvl1.setLvl1Nbr(34);
+        lvl1.setLvl1Name("D34 - Womens Apparel");
+        List<Lvl2> lvl2List = new ArrayList<>();
+        Lvl2 lvl2 = new Lvl2();
+        lvl2.setLvl2Nbr(6419);
+        lvl2.setLvl2Name("Plus Womens");
+        List<Lvl3> lvl3List = new ArrayList<>();
+        Lvl3 lvl3 = new Lvl3();
+        lvl3.setLvl3Nbr(12228);
+        List<Lvl4> lvl4List = new ArrayList<>();
+        Lvl4 lvl4 = new Lvl4();
+        lvl4.setLvl4Nbr(31507);
+        Fineline fineline = new Fineline();
+        List<Fineline> finelineList = new ArrayList<>();
+        fineline.setFinelineNbr(151);
+        fineline.setFinelineName("Women Hoodie");
+        finelineList.add(fineline);
+        Style style = new Style();
+        style.setStyleNbr("151_2_23_001");
+
+        CustomerChoice customerChoice = new CustomerChoice();
+        customerChoice.setCcId("151_2_23_001_001");
+        style.setCustomerChoices(Collections.singletonList(customerChoice));
+        fineline.setStyles(Collections.singletonList(style));
+        lvl4.setFinelines(finelineList);
+        lvl4List.add(lvl4);
+        lvl3.setLvl4List(lvl4List);
+        lvl3List.add(lvl3);
+        lvl2.setLvl3List(lvl3List);
+        lvl2List.add(lvl2);
+        lvl1.setLvl2List(lvl2List);
+        lvl1List.add(lvl1);
+        request.setLvl1List(lvl1List);
+        List<MerchCatPlan> merchCatPlans = SizeAndPackTest.getMerchCatPlanSet();
+        doReturn(merchCatPlans)
+                .when(merchCatPlanRepository).findMerchCatPlanByMerchCatPlanId_planIdAndMerchCatPlanId_lvl0NbrAndMerchCatPlanId_lvl1NbrAndMerchCatPlanId_lvl2NbrAndMerchCatPlanId_lvl3Nbr(471l, 50000, 34,6419,12228);
+        //Act
+        Set<MerchCatPlan> merchCatPlanSet= sizeAndPackDeletePlanService.updateMerchCatPlan(request,lvl1,lvl2,lvl3,fineline,merchCatPlanRepository);
+        assertTrue(merchCatPlanSet.size()==0);
+    }
+
+
+}
--- src/test/java/com/walmart/aex/sp/util/SizeAndPackTest.java
+++ src/test/java/com/walmart/aex/sp/util/SizeAndPackTest.java
@@ -5,8 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import com.walmart.aex.sp.dto.packoptimization.Fineline;
+import com.walmart.aex.sp.entity.*;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,5 +21,125 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class SizeAndPackTest {
+
+    private static Integer lvl0Nbr = 50000;
+    private static Integer lvl1Nbr = 34;
+    private static Integer lvl2Nbr = 6419;
+    private static Integer lvl3Nbr = 12228;
+    private static Integer lvl4Nbr = 31507;
+    private static Integer fineline1Nbr = 151;
+    private static String style1Nbr = "151_2_23_001";
+    private static String ccId = "151_2_23_001_001";
+
+    public static List<MerchCatPlan> getMerchCatPlanSet() {
+        List<MerchCatPlan> merchCatPlanSet = new ArrayList<>();
+        merchCatPlanSet.add(getMerchCatPlan());
+        return merchCatPlanSet;
+    }
+
+    public static MerchCatPlan getMerchCatPlan() {
+        MerchCatPlan merchCatPlan = new MerchCatPlan();
+        merchCatPlan.setMerchCatPlanId(getMerchCatPlanId());
+        merchCatPlan.setSubCatPlans(getSubCatPlanSet());
+        return merchCatPlan;
+    }
+
+    public static Set<SubCatPlan> getSubCatPlanSet() {
+
+        Set<SubCatPlan> subCatPlanSet = new HashSet<>();
+        subCatPlanSet.add(getSubCatPlan());
+        return subCatPlanSet;
+    }
+
+    public static SubCatPlan getSubCatPlan() {
+        SubCatPlan subCatPlan = new SubCatPlan();
+        SubCatPlanId subCatPlanId = getSubCatPlanId();
+        subCatPlan.setSubCatPlanId(subCatPlanId);
+        subCatPlan.setFinelinePlans(getFinelinePlanSet());
+        return subCatPlan;
+    }
+
+
+
+    public static Set<FinelinePlan> getFinelinePlanSet() {
+        Set<FinelinePlan> finelinePlanSet = new HashSet<>();
+        finelinePlanSet.add(getFinelinePlan());
+        return finelinePlanSet;
+    }
+
+    public static FinelinePlan getFinelinePlan() {
+        FinelinePlan finelinePlan = new FinelinePlan();
+        FinelinePlanId finelinePlanId = getFinelinePlanId();
+        finelinePlan.setFinelinePlanId(finelinePlanId);
+        finelinePlan.setStylePlans(getStylePlanSet());
+        return finelinePlan;
+    }
+
+
+
+    public static Set<StylePlan> getStylePlanSet() {
+        Set<StylePlan> stylePlanSet = new HashSet<>();
+        stylePlanSet.add(getStylePlan());
+        return stylePlanSet;
+    }
+
+    public static StylePlan getStylePlan() {
+        StylePlan stylePlan = new StylePlan();
+        StylePlanId stylePlanId = getStylePlanId();
+        stylePlan.setStylePlanId(stylePlanId);
+        stylePlan.setCustChoicePlans(getCustChoicePlanSet());
+        return stylePlan;
+    }
+
+
+    public static Set<CustChoicePlan> getCustChoicePlanSet() {
+        Set<CustChoicePlan> custChoicePlanSet = new HashSet<>();
+        custChoicePlanSet.add(getCustChoicePlan());
+        return custChoicePlanSet;
+    }
+
+    public static CustChoicePlan getCustChoicePlan() {
+        CustChoicePlan custChoicePlan = new CustChoicePlan();
+        CustChoicePlanId custChoicePlanId = getCustChoicePlanId();
+        custChoicePlan.setCustChoicePlanId(custChoicePlanId);
+        return custChoicePlan;
+    }
+
+    private static CustChoicePlanId getCustChoicePlanId() {
+        CustChoicePlanId custChoicePlanId = new CustChoicePlanId();
+        custChoicePlanId.setStylePlanId(getStylePlanId());
+        custChoicePlanId.setCcId(ccId);
+        return custChoicePlanId;
+    }
+
+    @NotNull
+    private static StylePlanId getStylePlanId() {
+        StylePlanId stylePlanId = new StylePlanId();
+        stylePlanId.setFinelinePlanId(getFinelinePlanId());
+        stylePlanId.setStyleNbr(style1Nbr);
+        return stylePlanId;
+    }
+    private static FinelinePlanId getFinelinePlanId() {
+        FinelinePlanId finelinePlanId = new FinelinePlanId();
+        finelinePlanId.setSubCatPlanId(getSubCatPlanId());
+        finelinePlanId.setFinelineNbr(fineline1Nbr);
+        return finelinePlanId;
+    }
+    private static SubCatPlanId getSubCatPlanId() {
+        SubCatPlanId subCatPlanId = new SubCatPlanId();
+        subCatPlanId.setMerchCatPlanId(getMerchCatPlanId());
+        subCatPlanId.setLvl4Nbr(lvl4Nbr);
+        return subCatPlanId;
+    }
+
+    public static MerchCatPlanId getMerchCatPlanId(){
+        MerchCatPlanId merchCatPlanId = new MerchCatPlanId();
+        merchCatPlanId.setPlanId(346l);
+        merchCatPlanId.setLvl0Nbr(lvl0Nbr);
+        merchCatPlanId.setLvl1Nbr(lvl1Nbr);
+        merchCatPlanId.setLvl2Nbr(lvl2Nbr);
+        merchCatPlanId.setLvl3Nbr(lvl3Nbr);
+        return merchCatPlanId;
+    }
 
 }
