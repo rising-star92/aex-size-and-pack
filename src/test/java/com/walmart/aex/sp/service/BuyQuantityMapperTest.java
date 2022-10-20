dce4b966bde40ad39eef7a9f0267cc05e7c23924
rising-star92 <ryan.service.1001@gmail.com>
2022-10-20 17:24:01 +0530

    Adding testcases to BuyQtyMapper

--- src/test/java/com/walmart/aex/sp/service/BuyQuantityMapperTest.java
+++ src/test/java/com/walmart/aex/sp/service/BuyQuantityMapperTest.java
@@ -85,6 +85,33 @@ public class BuyQuantityMapperTest {
     }
 
     @Test
+    public void testMapBuyQntyUpdateFineline() {
+
+
+        buyQntyResponseDTO=new BuyQntyResponseDTO();
+        buyQntyResponseDTO.setChannelId(1);
+        buyQntyResponseDTO.setReplnQty(14);
+        buyQntyResponseDTO.setInitialSetQty(10);
+        buyQntyResponseDTO.setFinelineNbr(123);
+        buyQntyResponseDTO.setPlanId(planId);
+        buyQntyResponseDTO.setBuyQty(654);
+        buyQntyResponseDTO.setBumpPackQty(12);
+        buyQntyResponseDTO.setAdjReplnQty(234);
+        buyQntyResponseDTO.setBumpPackQty(654);
+        buyQntyResponseDTO.setCcBumpQty(64);
+        buyQntyResponseDTO.setCcFlowStrategy(765);
+        buyQntyResponseDTO.setLvl2Nbr(12);
+        buyQntyResponseDTO.setLvl3Nbr(13);
+        buyQntyResponseDTO.setLvl4Nbr(11);
+        buyQntyResponseDTO.setReplnQty(14);
+        buyQntyResponseDTO.setInitialSetQty(10);
+        BuyQtyResponse fetchFineLineResponse= getBuyQtyResponse(buyQntyResponseDTO,null);
+        buyQunatityMapper.mapBuyQntyLvl2Sp(buyQntyResponseDTO,fetchFineLineResponse,null);
+        assertNotNull(fetchFineLineResponse);
+        assertEquals(471l,fetchFineLineResponse.getPlanId());
+    }
+
+    @Test
     public void testMapBuyQntyLvl2SpLvl3Metrics() {
 
         BuyQtyResponse fetchFineLineResponse= new BuyQtyResponse();
@@ -172,11 +199,90 @@ public class BuyQuantityMapperTest {
         assertEquals(metricsDto.getAvgSizeProfilePct(),100);
     }
 
+    @Test
+    public void testMapBuyQntyUpdateStyleCcMetrics() {
+
+        buyQntyResponseDTO=new BuyQntyResponseDTO();
+        buyQntyResponseDTO.setPlanId(12L);
+        buyQntyResponseDTO.setLvl2Nbr(12);
+        buyQntyResponseDTO.setLvl3Nbr(13);
+        buyQntyResponseDTO.setLvl4Nbr(11);
+        buyQntyResponseDTO.setFinelineNbr(2816);
+        buyQntyResponseDTO.setStyleNbr("34_2816_2_19_2");
+        buyQntyResponseDTO.setCcId("34_2816_2_19_2_CHARCOAL GREY HEATHER");
+        buyQntyResponseDTO.setStyleFlowStrategy(1);
+        buyQntyResponseDTO.setStyleMerchCode(1);
+        buyQntyResponseDTO.setStyleBumpQty(650);
+        buyQntyResponseDTO.setStyleIsQty(50);
+        buyQntyResponseDTO.setStyleBuyQty(700);
+        buyQntyResponseDTO.setCcFlowStrategy(1);
+        buyQntyResponseDTO.setCcMerchCode(1);
+        buyQntyResponseDTO.setCcBumpQty(650);
+        buyQntyResponseDTO.setCcIsQty(50);
+        buyQntyResponseDTO.setCcBuyQty(700);
+        buyQntyResponseDTO.setCcReplnQty(234);
+        buyQntyResponseDTO.setChannelId(1);
+
+
+        List<StyleDto> styleDtoList= new ArrayList<>();
+        StyleDto styleDto = new StyleDto();
+        List<CustomerChoiceDto> customerChoiceDtoList = new ArrayList<>();
+        CustomerChoiceDto customerChoiceDto= new CustomerChoiceDto();
+        customerChoiceDto.setCcId(buyQntyResponseDTO.getCcId());
+        customerChoiceDto.setChannelId(1);
+        customerChoiceDto.setMetrics(getMetricsInputs());
+        customerChoiceDtoList.add(customerChoiceDto);
+        styleDto.setStyleNbr(buyQntyResponseDTO.getStyleNbr());
+        styleDto.setChannelId(1);
+        styleDto.setMetrics(getMetricsInputs());
+        styleDto.setCustomerChoices(customerChoiceDtoList);
+        styleDtoList.add(styleDto);
+        BuyQtyResponse fetchFineLineResponse= getBuyQtyResponse(buyQntyResponseDTO,styleDtoList);
+        buyQunatityMapper.mapBuyQntyLvl2Sp(buyQntyResponseDTO,fetchFineLineResponse,2816);
+        assertEquals(12,fetchFineLineResponse.getPlanId());
+    }
+
     private BuyQtyResponse sizeProfileResponseFromJson(String filename) throws IOException {
         return mapper.readValue(readJsonFileAsString(filename), BuyQtyResponse.class);
     }
 
     private String readJsonFileAsString(String fileName) throws IOException {
         return new String(Files.readAllBytes(Paths.get("src/test/resources/data/" + fileName + ".json")));
+    }
+
+    private BuyQtyResponse getBuyQtyResponse(BuyQntyResponseDTO buyQntyResponseDTO, List<StyleDto> styleDtoList)
+    {
+        BuyQtyResponse fetchFineLineResponse= new BuyQtyResponse();
+        List<Lvl3Dto> lvl3List = new ArrayList<>();
+        List<Lvl4Dto> lvl4DtoList = new ArrayList<>();
+        Lvl4Dto lvl4Dto = new Lvl4Dto();
+        List<FinelineDto> finelineDtoList = new ArrayList<>();
+        FinelineDto finelineDto= new FinelineDto();
+        finelineDto.setFinelineNbr(buyQntyResponseDTO.getFinelineNbr());
+        finelineDto.setChannelId(1);
+        finelineDto.setMetrics(getMetricsInputs());
+        finelineDto.setStyles(styleDtoList);
+        finelineDtoList.add(finelineDto);
+        lvl4Dto.setLvl4Nbr(buyQntyResponseDTO.getLvl4Nbr());
+        lvl4Dto.setFinelines(finelineDtoList);
+        lvl4DtoList.add(lvl4Dto);
+        Lvl3Dto lvl3Dto = new Lvl3Dto();
+        lvl3Dto.setLvl3Nbr(buyQntyResponseDTO.getLvl3Nbr());
+        lvl3Dto.setLvl4List(lvl4DtoList);
+        lvl3List.add(lvl3Dto);
+        fetchFineLineResponse.setLvl3List(lvl3List);
+        return fetchFineLineResponse;
+    }
+
+    private MetricsDto getMetricsInputs()
+    {
+        MetricsDto metricsDto= new MetricsDto();
+        metricsDto.setReplenishmentPacks(12);
+        metricsDto.setFinalBuyQty(123);
+        metricsDto.setBuyQty(234);
+        metricsDto.setBumpPackQty(321);
+        metricsDto.setFinalInitialSetQty(342);
+        metricsDto.setFinalReplenishmentQty(321);
+        return metricsDto;
     }
 }
