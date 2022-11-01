d96302f6c966963c7e351353ebc426dbbcbd6bc8
rising-star92 <ryan.service.1001@gmail.com>
2022-11-01 14:26:52 -0400

    removed

--- src/test/java/com/walmart/aex/sp/service/CalculateOnlineFinelineBuyQuantityTest.java
+++ src/test/java/com/walmart/aex/sp/service/CalculateOnlineFinelineBuyQuantityTest.java
@@ -4,19 +4,11 @@ import com.walmart.aex.sp.dto.bqfp.BQFPResponse;
 import com.walmart.aex.sp.dto.buyquantity.*;
 import com.walmart.aex.sp.entity.MerchCatgReplPack;
 import com.walmart.aex.sp.exception.SizeAndPackException;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.annotation.DirtiesContext;
 import java.io.IOException;
 import java.util.List;
 import static org.junit.Assert.assertEquals;
 
-
-@ExtendWith(MockitoExtension.class)
-@DirtiesContext(methodMode = DirtiesContext.MethodMode.AFTER_METHOD)
-@Slf4j
 public class CalculateOnlineFinelineBuyQuantityTest extends CalculateFinelineBuyQuantityTest {
 
     @Test
