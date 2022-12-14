a7bcafbe31527cf58e4c36c34ca7a060365eacea
rising-star92 <ryan.service.1001@gmail.com>
2022-12-14 09:17:59 -0600

    Optimized the code

--- src/main/java/com/walmart/aex/sp/service/PackOptimizationService.java
+++ src/main/java/com/walmart/aex/sp/service/PackOptimizationService.java
@@ -225,9 +225,7 @@ public class PackOptimizationService {
     }
 
     private String getColorCombinationId(Set<String> colorCombinationIds) {
-        if(CollectionUtils.isEmpty(colorCombinationIds)){
-            return DEFAULT_COLOR_COMBINATION_ID;
-        }
+        if(CollectionUtils.isEmpty(colorCombinationIds)) return DEFAULT_COLOR_COMBINATION_ID;
         int nextColorCombinationId =  colorCombinationIds.stream()
                 .filter(id -> !StringUtils.isEmpty(id) && StringUtils.isNumeric(id))
                 .mapToInt(Integer::valueOf)
