0247429a7d6e4c86d0c7b0fc65e3b5d89ed78cab
rising-star92 <ryan.service.1001@gmail.com>
2023-03-10 13:18:50 -0500

    reverting local changes

--- src/main/java/com/walmart/aex/sp/service/StrategyFetchService.java
+++ src/main/java/com/walmart/aex/sp/service/StrategyFetchService.java
@@ -38,14 +38,14 @@ public class StrategyFetchService {
         Map<String, String> headers = getHeaderForStrategy();
         Map<String, Object> data = new HashMap<>();
         data.put("sizeProfileRequest", buyQtyRequest);
-        return (BuyQtyResponse) post(graphQLProperties.getSizeProfileUrl(), "query ($sizeProfileRequest:SizeProfileRequest!)\n {\n getCcSizeClus(sizeProfileRequest:  $sizeProfileRequest)\n {\n planId\n lvl0Nbr\n lvl1Nbr\n lvl2Nbr\n lvl3List{\n lvl3Nbr\n lvl4List{\n lvl4Nbr\n finelines{\n finelineNbr\n styles{\n styleNbr\n customerChoices{\n ccId\n clusters{\n clusterID\n sizes{\n ahsSizeId\n sizeDesc\n metrics{\n sizeProfilePct\n adjSizeProfilePct\n avgSizeProfilePct\n adjAvgSizeProfilePct\n }}}}}}}}}}", headers, data, Payload::getGetCcSizeClus);
+        return (BuyQtyResponse) post(graphQLProperties.getSizeProfileUrl(), graphQLProperties.getSizeProfileQuery(), headers, data, Payload::getGetCcSizeClus);
     }
 
     public BuyQtyResponse getBuyQtyDetailsForFinelines(BuyQtyRequest buyQtyRequest) throws SizeAndPackException {
 
         Map<String, String> headers = getHeaderForStrategy();
         Map<String, Object> data = getBuyQtyRequest(buyQtyRequest);
-        return (BuyQtyResponse) post(graphQLProperties.getSizeProfileUrl(), "query ($planId:Int!,$channel:String!)\n {\n getFinelinesWithSizeAssociation(planId : $planId, channel : $channel)\n {\n planId\n lvl0Nbr\n lvl1Nbr\n lvl2Nbr\n lvl3List{\n lvl3Nbr\n lvl4List{\n lvl4Nbr\n finelines{\n finelineNbr\n }}}}}", headers, data, Payload::getGetFinelinesWithSizeAssociation);
+        return (BuyQtyResponse) post(graphQLProperties.getSizeProfileUrl(), graphQLProperties.getBuyQtyFinelinesSizeQuery(), headers, data, Payload::getGetFinelinesWithSizeAssociation);
     }
 
     public BuyQtyResponse getBuyQtyDetailsForStylesCc(BuyQtyRequest buyQtyRequest, Integer finelineNbr) throws SizeAndPackException {
@@ -54,7 +54,7 @@ public class StrategyFetchService {
         Map<String, Object> data = getBuyQtyRequest(buyQtyRequest);
         data.put("finelineNbr", finelineNbr);
 
-        return (BuyQtyResponse) post(graphQLProperties.getSizeProfileUrl(), "query ($planId:Int!,$channel:String!,$finelineNbr:Int!)\n {\n getStylesCCsWithSizeAssociation(planId : $planId, channel : $channel, finelineNbr: $finelineNbr)\n {\n planId\n lvl0Nbr\n lvl1Nbr\n lvl2Nbr\n lvl3List{\n lvl3Nbr\n lvl4List{\n lvl4Nbr\n finelines{\n finelineNbr\n styles{\n styleNbr\n customerChoices{\n ccId\n }}}}}}}", headers, data, Payload::getGetStylesCCsWithSizeAssociation);
+        return (BuyQtyResponse) post(graphQLProperties.getSizeProfileUrl(), graphQLProperties.getBuyQtyStyleCcSizeQuery(), headers, data, Payload::getGetStylesCCsWithSizeAssociation);
     }
 
     private Object post(String url, String query, Map<String, String> headers, Map<String, Object> data, Function<Payload, ?> responseFunc) throws SizeAndPackException {
@@ -81,7 +81,7 @@ public class StrategyFetchService {
         Map<String, Object> data = new HashMap<>();
         data.put("request", request);
 
-        return (APResponse) post(graphQLProperties.getAssortProductUrl(), "query($request:RFASizePackRequest!){getRFADataFromSizePack(rfaSizePackRequest:$request){rfaSizePackData{rpt_lvl_0_nbr rpt_lvl_1_nbr rpt_lvl_2_nbr rpt_lvl_3_nbr rpt_lvl_4_nbr fineline_nbr style_nbr customer_choice fixture_type fixture_group color_family size_cluster_id volume_group_cluster_id store_list store_cnt plan_id_partition}}}", headers, data, Payload::getGetRFADataFromSizePack);
+        return (APResponse) post(graphQLProperties.getAssortProductUrl(), graphQLProperties.getAssortProductRFAQuery(), headers, data, Payload::getGetRFADataFromSizePack);
     }
 
     public BuyQtyResponse getAllCcSizeProfiles(BuyQtyRequest buyQtyRequest) throws SizeAndPackException
@@ -89,7 +89,7 @@ public class StrategyFetchService {
         Map<String, String> headers = getHeaderForStrategy();
         Map<String, Object> data = new HashMap<>();
         data.put("sizeProfileRequest", buyQtyRequest);
-        return (BuyQtyResponse) post(graphQLProperties.getSizeProfileUrl(), "query ($sizeProfileRequest:SizeProfileRequest!)\n {\n getAllCcSizeClus(sizeProfileRequest:  $sizeProfileRequest)\n {\n planId\n lvl0Nbr\n lvl1Nbr\n lvl2Nbr\n lvl3List{\n lvl3Nbr\n lvl4List{\n lvl4Nbr\n finelines{\n finelineNbr\n merchMethods{\n fixtureTypeRollupId\n merchMethod\n merchMethodCode\n }\n metrics{\n sizeProfilePct\n adjSizeProfilePct\n avgSizeProfilePct\n adjAvgSizeProfilePct\n }\n clusters{\n clusterID\n sizes{\n ahsSizeId\n sizeDesc\n metrics{\n sizeProfilePct\n adjSizeProfilePct\n avgSizeProfilePct\n adjAvgSizeProfilePct\n }\n }\n }\n styles{\n styleNbr\n metrics{\n sizeProfilePct\n adjSizeProfilePct\n avgSizeProfilePct\n adjAvgSizeProfilePct\n }\n clusters{\n clusterID\n sizes{\n ahsSizeId\n sizeDesc\n metrics{\n sizeProfilePct\n adjSizeProfilePct\n avgSizeProfilePct\n adjAvgSizeProfilePct\n }\n }\n }\n customerChoices{\n ccId\n metrics{\n sizeProfilePct\n adjSizeProfilePct\n avgSizeProfilePct\n adjAvgSizeProfilePct\n }\n clusters{\n clusterID\n sizes{\n ahsSizeId\n sizeDesc\n metrics{\n sizeProfilePct\n adjSizeProfilePct\n avgSizeProfilePct\n adjAvgSizeProfilePct\n }}}}}}}}}}", headers, data, Payload::getGetAllCcSizeClus);
+        return (BuyQtyResponse) post(graphQLProperties.getSizeProfileUrl(), graphQLProperties.getAllCcSizeProfileQuery(), headers, data, Payload::getGetAllCcSizeClus);
     }
 
     public StrategyVolumeDeviationResponse getStrategyVolumeDeviation(List<StrategyVolumeDeviationRequest> strategyVolumeDeviationRequests) throws SizeAndPackException
@@ -97,7 +97,7 @@ public class StrategyFetchService {
         Map<String, String> headers = getHeaderForStrategy();
         Map<String, Object> data = new HashMap<>();
         data.put("volumeDeviationRequests", strategyVolumeDeviationRequests);
-        return (StrategyVolumeDeviationResponse) post(graphQLProperties.getSizeProfileUrl(), "query($volumeDeviationRequests:[VolumeDeviationRequests]) {getVolumeDeviationStrategySelection (request: {volumeDeviationRequestsList: $volumeDeviationRequests }) { finelines { finelineId finelineName lvl0Nbr lvl1Nbr lvl2Nbr lvl3Nbr lvl4Nbr planId volumeDeviationLevel } } }", headers, data, Payload::getGetVolumeDeviationStrategySelection);
+        return (StrategyVolumeDeviationResponse) post(graphQLProperties.getSizeProfileUrl(), graphQLProperties.getStrategyVolumeDeviationLevel(), headers, data, Payload::getGetVolumeDeviationStrategySelection);
     }
 
     public Map<String, String> getHeaderForStrategy() {
