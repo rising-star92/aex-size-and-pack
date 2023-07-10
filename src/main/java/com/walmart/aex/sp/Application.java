36327b7147fd0266c8bfe17ecd670e63c2df37c4
rising-star92 <ryan.service.1001@gmail.com>
2023-07-10 12:55:52 -0700

    updated swagger version

--- src/main/java/com/walmart/aex/sp/Application.java
+++ src/main/java/com/walmart/aex/sp/Application.java
@@ -1,15 +1,14 @@
 package com.walmart.aex.sp;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.retry.annotation.EnableRetry;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @EnableRetry
 @ComponentScan("com.walmart.platform.txn.springboot.filters")
@@ -22,10 +21,14 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-    
+
+
     @Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.basePackage("com.walmart.aex.sp"))
-				.paths(PathSelectors.any()).build();
-	}
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().info(new Info().title("aex-size-and-pack service")
+                .description("This page lists all APIs of Size-and-Pack Service")
+                .termsOfService("http://swagger.io/terms/")
+                .license(new License().name("Apache 2.0")
+                        .url("http://springdoc.org")));
+    }
 }
