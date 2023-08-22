daac220651da74771a8b83c93026028acf4a0859
rising-star92 <ryan.service.1001@gmail.com>
2023-08-22 10:51:58 +0530

    Revert "Revert "MTAP-8376 : Modify the packopt graphql endpoint to send multiple Errors and run_status_long_desc to send to UI""

--- src/main/java/com/walmart/aex/sp/dto/packoptimization/RunOptimization.java
+++ src/main/java/com/walmart/aex/sp/dto/packoptimization/RunOptimization.java
@@ -4,12 +4,12 @@ package com.walmart.aex.sp.dto.packoptimization;
 import lombok.Data;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class RunOptimization {
     private String name;
-    private String returnMessage;
-    private String runStatusLongDesc;
+    private List<String> runStatusLongDesc;
     private Date startTs;
     private Date endTs;
     private Integer runStatusCode;
