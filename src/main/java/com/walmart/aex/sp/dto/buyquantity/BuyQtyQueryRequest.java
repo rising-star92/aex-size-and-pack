4b743da036238efbb08f06bfe0f693eb4b0f167d
rising-star92 <ryan.service.1001@gmail.com>
2023-09-18 11:22:39 -0400

    added dummy API for query performance test

---
+++ src/main/java/com/walmart/aex/sp/dto/buyquantity/BuyQtyQueryRequest.java
@@ -0,0 +1,28 @@
+package com.walmart.aex.sp.dto.buyquantity;
+
+import com.walmart.aex.sp.dto.assortproduct.ColorDefinition;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BuyQtyQueryRequest {
+    private Integer planId;
+    private Integer lvl0;
+    private Integer lvl1;
+    private Integer lvl2;
+    private Integer lvl3;
+    private Integer lvl4;
+    private Integer finelineNbr;
+    private Integer likeFinelineNbr;
+    private Integer likeLvl1;
+    private Integer likeLvl3;
+    private Integer likeLvl4;
+    private String colors;
+    private Integer fiscalYear;
+    private String seasonCode;
+    private String volDeviation;
+}
