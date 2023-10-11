f305b67f0f176159df88272c2d7db447d4846b97
rising-star92 <ryan.service.1001@gmail.com>
2023-10-11 16:27:19 -0400

    updated the SpCustomerChoiceChannelFixtureSize.java entity

--- src/main/java/com/walmart/aex/sp/entity/SpCustomerChoiceChannelFixtureSize.java
+++ src/main/java/com/walmart/aex/sp/entity/SpCustomerChoiceChannelFixtureSize.java
@@ -19,7 +19,7 @@ public class SpCustomerChoiceChannelFixtureSize {
     @EqualsAndHashCode.Include
     private SpCustomerChoiceChannelFixtureSizeId spCustomerChoiceChannelFixtureSizeId;
 
-    @ManyToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "plan_id", referencedColumnName = "plan_id", nullable = false, insertable = false, updatable = false)
     @JoinColumn(name = "rpt_lvl_0_nbr", referencedColumnName = "rpt_lvl_0_nbr", nullable = false, insertable = false, updatable = false)
     @JoinColumn(name = "rpt_lvl_1_nbr", referencedColumnName = "rpt_lvl_1_nbr", nullable = false, insertable = false, updatable = false)
