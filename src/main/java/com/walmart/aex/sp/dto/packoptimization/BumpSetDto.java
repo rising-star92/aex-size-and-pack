dc2b1b7983e8a35740ebb959cec3ae045db00e71
rising-star92 <ryan.service.1001@gmail.com>
2023-10-13 18:34:13 -0500

    one last hurrah

--- src/main/java/com/walmart/aex/sp/dto/packoptimization/BumpSetDto.java
+++ src/main/java/com/walmart/aex/sp/dto/packoptimization/BumpSetDto.java
@@ -1,8 +1,10 @@
 package com.walmart.aex.sp.dto.packoptimization;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class BumpSetDto {
 
 	private Integer setNbr;
