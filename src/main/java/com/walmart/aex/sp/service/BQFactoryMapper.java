76807dac1e95e8618e92747936c6d7379a62ad88
rising-star92 <ryan.service.1001@gmail.com>
2024-01-03 08:32:25 -0600

    Update

--- src/main/java/com/walmart/aex/sp/service/BQFactoryMapper.java
+++ src/main/java/com/walmart/aex/sp/service/BQFactoryMapper.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -12,34 +13,37 @@ import java.util.stream.Collectors;
 public class BQFactoryMapper {
 
     public void setFactoriesForFinelines(List<FactoryDTO> factoryDTOS, BuyQtyResponse buyQtyResponse) {
-        buyQtyResponse.getLvl3List().forEach(lvl3Dto -> lvl3Dto.getLvl4List()
-                .forEach(lvl4Dto -> lvl4Dto.getFinelines()
-                        .forEach(finelineDto -> {
-                                    if (finelineDto.getMetrics() == null) {
-                                        finelineDto.setMetrics(new MetricsDto());
+        if (buyQtyResponse.getLvl3List() != null) {
+            buyQtyResponse.getLvl3List().forEach(lvl3Dto -> lvl3Dto.getLvl4List()
+                    .forEach(lvl4Dto -> lvl4Dto.getFinelines()
+                            .forEach(finelineDto -> {
+                                        if (finelineDto.getMetrics() == null) {
+                                            finelineDto.setMetrics(new MetricsDto());
+                                        }
+                                        finelineDto.getMetrics().setFactories(findFactoriesForFineline(finelineDto, factoryDTOS));
                                     }
-                                    finelineDto.getMetrics().setFactories(findFactoriesForFineline(finelineDto, factoryDTOS));
-                                }
-                        )));
+                            )));
+        }
     }
 
 
     public void setFactoriesForCCs(List<FactoryDTO> factoryDTOS, BuyQtyResponse buyQtyResponse) {
-        buyQtyResponse.getLvl3List().forEach(lvl3Dto -> lvl3Dto.getLvl4List()
-                .forEach(lvl4Dto -> lvl4Dto.getFinelines()
-                        .forEach(finelineDto -> finelineDto.getStyles().forEach(styleDto -> {
-                            if (styleDto.getMetrics() == null) {
-                                styleDto.setMetrics(new MetricsDto());
-                            }
-                            styleDto.getMetrics().setFactories(setFactoriesForStyle(styleDto, factoryDTOS));
-                            styleDto.getCustomerChoices().forEach(customerChoiceDto -> {
-                                if (customerChoiceDto.getMetrics() == null) {
-                                    customerChoiceDto.setMetrics(new MetricsDto());
+        if (buyQtyResponse.getLvl3List() != null) {
+            buyQtyResponse.getLvl3List().forEach(lvl3Dto -> lvl3Dto.getLvl4List()
+                    .forEach(lvl4Dto -> lvl4Dto.getFinelines()
+                            .forEach(finelineDto -> finelineDto.getStyles().forEach(styleDto -> {
+                                if (styleDto.getMetrics() == null) {
+                                    styleDto.setMetrics(new MetricsDto());
                                 }
-                                customerChoiceDto.getMetrics().setFactories(findFactoriesForCC(customerChoiceDto, factoryDTOS));
-                            });
-                        }))));
-
+                                styleDto.getMetrics().setFactories(setFactoriesForStyle(styleDto, factoryDTOS));
+                                styleDto.getCustomerChoices().forEach(customerChoiceDto -> {
+                                    if (customerChoiceDto.getMetrics() == null) {
+                                        customerChoiceDto.setMetrics(new MetricsDto());
+                                    }
+                                    customerChoiceDto.getMetrics().setFactories(findFactoriesForCC(customerChoiceDto, factoryDTOS));
+                                });
+                            }))));
+        }
     }
 
     private List<FactoryDTO> findFactoriesForFineline(FinelineDto finelineDto, List<FactoryDTO> factoryDTOS) {
