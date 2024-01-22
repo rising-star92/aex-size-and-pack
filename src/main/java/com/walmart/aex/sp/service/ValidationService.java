6d7f4b0eada8b0f4557a4af0d69cdecadb3a5a39
rising-star92 <ryan.service.1001@gmail.com>
2024-01-22 17:04:27 -0600

    MTAP-9508 fixed method name

--- src/main/java/com/walmart/aex/sp/service/ValidationService.java
+++ src/main/java/com/walmart/aex/sp/service/ValidationService.java
@@ -3,11 +3,9 @@ package com.walmart.aex.sp.service;
 import com.walmart.aex.sp.dto.assortproduct.APResponse;
 import com.walmart.aex.sp.dto.bqfp.BQFPResponse;
 import com.walmart.aex.sp.dto.buyquantity.CustomerChoiceDto;
-import com.walmart.aex.sp.dto.buyquantity.ValidationCode;
-import com.walmart.aex.sp.dto.replenishment.MerchMethodsDto;
 
 import java.util.List;
 
 public interface ValidationService {
-    public ValidationCode validateCalculateBuyQuantityData(List<MerchMethodsDto> merchMethodsDtos, APResponse apResponse, BQFPResponse bqfpResponse, String styleNbr, CustomerChoiceDto customerChoiceDto);
+    List<Integer> validateCalculateBuyQuantityInputData(APResponse apResponse, BQFPResponse bqfpResponse, String styleNbr, CustomerChoiceDto customerChoiceDto);
 }
