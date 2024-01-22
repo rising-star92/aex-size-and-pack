be5a42e1715a5270f3d990f86c8c6822bdcd38e1
rising-star92 <ryan.service.1001@gmail.com>
2024-01-22 10:07:57 -0600

    MTAP-9508 Added structure for db column and created entry point for validation service

---
+++ src/main/java/com/walmart/aex/sp/service/ValidationService.java
@@ -0,0 +1,13 @@
+package com.walmart.aex.sp.service;
+
+import com.walmart.aex.sp.dto.assortproduct.APResponse;
+import com.walmart.aex.sp.dto.bqfp.BQFPResponse;
+import com.walmart.aex.sp.dto.buyquantity.CustomerChoiceDto;
+import com.walmart.aex.sp.dto.buyquantity.ValidationCode;
+import com.walmart.aex.sp.dto.replenishment.MerchMethodsDto;
+
+import java.util.List;
+
+public interface ValidationService {
+    public List<ValidationCode> validateCalculateBuyQuantityData(List<MerchMethodsDto> merchMethodsDtos, APResponse apResponse, BQFPResponse bqfpResponse, Integer styleNbr, CustomerChoiceDto customerChoiceDto);
+}
---
+++ src/main/java/com/walmart/aex/sp/service/impl/ValidationServiceImpl.java
@@ -0,0 +1,23 @@
+package com.walmart.aex.sp.service.impl;
+
+import com.walmart.aex.sp.dto.assortproduct.APResponse;
+import com.walmart.aex.sp.dto.bqfp.BQFPResponse;
+import com.walmart.aex.sp.dto.buyquantity.CustomerChoiceDto;
+import com.walmart.aex.sp.dto.buyquantity.ValidationCode;
+import com.walmart.aex.sp.dto.replenishment.MerchMethodsDto;
+import com.walmart.aex.sp.service.ValidationService;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ValidationServiceImpl implements ValidationService {
+
+    //All validation service per dataset - RFAValidationService, BQFPValidationService
+
+    @Override
+    public List<ValidationCode> validateCalculateBuyQuantityData(List<MerchMethodsDto> merchMethodsDtos, APResponse apResponse, BQFPResponse bqfpResponse, Integer styleNbr, CustomerChoiceDto customerChoiceDto) {
+        // separate method to call each individual validation service
+        return Collections.emptyList();
+    }
+
+}
