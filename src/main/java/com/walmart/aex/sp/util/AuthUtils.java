4c959d86ee8634fa76f946cc89142aa2fc716200
rising-star92 <ryan.service.1001@gmail.com>
2024-02-22 14:53:56 -0500

    added authutil

---
+++ src/main/java/com/walmart/aex/sp/util/AuthUtils.java
@@ -0,0 +1,23 @@
+package com.walmart.aex.sp.util;
+
+import com.walmart.aex.security.model.AuditUserInfo;
+import com.walmart.aex.security.model.User;
+import lombok.experimental.UtilityClass;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+@UtilityClass
+public class AuthUtils {
+    public static String getAuthorizationToken() {
+        User authenticatedUser = (User) Optional.ofNullable(SecurityContextHolder.getContext()).flatMap(context -> Optional.ofNullable(context.getAuthentication()).map(Authentication::getPrincipal)).orElse(null);
+        return Optional.ofNullable(authenticatedUser).flatMap(authUser -> Optional.ofNullable(authUser.getAuditUserInfo()).map(AuditUserInfo::getUsernameToken)).orElse(null);
+    }
+
+    public static String getAuthenticatedUserName() {
+        User authenticatedUser = (User) Optional.ofNullable(SecurityContextHolder.getContext()).flatMap(context -> Optional.ofNullable(context.getAuthentication()).map(Authentication::getPrincipal)).orElse(null);
+        return Optional.ofNullable(authenticatedUser).flatMap(authUser -> Optional.ofNullable(authUser.getAuditUserInfo()).map(AuditUserInfo::getAuthenticatedUserName)).orElse(null);
+    }
+
+}
