a8fb8c116cd10946d930bd233acf6de4d6d8fc4a
rising-star92 <ryan.service.1001@gmail.com>
2024-03-07 16:13:34 -0500

    removed unused annotations

--- src/main/java/com/walmart/aex/sp/dto/deptadminrule/PlanAdminRuleResponse.java
+++ src/main/java/com/walmart/aex/sp/dto/deptadminrule/PlanAdminRuleResponse.java
@@ -1,12 +1,8 @@
 package com.walmart.aex.sp.dto.deptadminrule;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class PlanAdminRuleResponse {
     private Long planId;
     private Integer deptNbr;
