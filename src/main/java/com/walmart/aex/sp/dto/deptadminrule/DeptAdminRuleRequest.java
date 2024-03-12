dbad79941c0e7f5df1098ce3c4446b391ee14448
rising-star92 <ryan.service.1001@gmail.com>
2024-03-12 13:35:42 -0400

    remove unsued imports

--- src/main/java/com/walmart/aex/sp/dto/deptadminrule/DeptAdminRuleRequest.java
+++ src/main/java/com/walmart/aex/sp/dto/deptadminrule/DeptAdminRuleRequest.java
@@ -2,13 +2,11 @@ package com.walmart.aex.sp.dto.deptadminrule;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
-@EqualsAndHashCode(callSuper=true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class DeptAdminRuleRequest extends ReplItemResponse {
