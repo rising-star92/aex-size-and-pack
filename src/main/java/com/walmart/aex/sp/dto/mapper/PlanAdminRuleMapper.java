8281994f5cb0895611a4e2337efeb1e9d58beff6
rising-star92 <ryan.service.1001@gmail.com>
2024-03-12 11:58:01 -0400

    updated pr

--- src/main/java/com/walmart/aex/sp/dto/mapper/PlanAdminRuleMapper.java
+++ src/main/java/com/walmart/aex/sp/dto/mapper/PlanAdminRuleMapper.java
@@ -4,19 +4,15 @@ import com.walmart.aex.sp.dto.deptadminrule.PlanAdminRuleRequest;
 import com.walmart.aex.sp.dto.deptadminrule.PlanAdminRuleResponse;
 import com.walmart.aex.sp.entity.PlanAdminRule;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Set;
-
-import static com.walmart.aex.security.service.UserDetailsService.getAuthenticatedUserName;
 
 @Mapper
 public interface PlanAdminRuleMapper {
     PlanAdminRuleMapper mapper = Mappers.getMapper(PlanAdminRuleMapper.class);
     List<PlanAdminRule> mapRequestToEntity(List<PlanAdminRuleRequest> requests);
-
     Set<PlanAdminRuleResponse> mapEntityToResponse(List<PlanAdminRule> entity);
+
 }
